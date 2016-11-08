3e97a55fb10ade0f50d4c215d09e502cc94825eb
Nikolay Kolbun <Mykola.Kolbun@gmail.com>
2016-11-08 23:36:28 +0400

    Починил создание релиз апк

--- ParkeonService/app/src/main/java/ua/com/alternatiview/parkeonservice/MapsActivity.java
+++ ParkeonService/app/src/main/java/ua/com/alternatiview/parkeonservice/MapsActivity.java
@@ -1,11 +1,13 @@
 package ua.com.alternatiview.parkeonservice;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,6 +28,8 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
     DB_connect con = new DB_connect();
     Boolean isDraggable = true;
     public static LatLng newPoint;
+    private AlertDialog.Builder dialogConfirmLocation, dialogChangeStatus, dialogAddRemark;
+    int intStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +42,6 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
         String androidID = Settings.Secure.getString(getBaseContext().getContentResolver(), Settings.Secure.ANDROID_ID);
         machinesList = con.GetTempDevices(androidID);
         mapFragment.getMapAsync(this);
-
-
     }
 
     @Override
@@ -51,14 +53,21 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
         for (int i = 0; i < machinesList.size(); i++) {
             LatLng point = new LatLng(machinesList.get(i).latitude, machinesList.get(i).longitude);
             Marker m;
-            if (machinesList.get(i).status>0) {
+            if (machinesList.get(i).status==1) {
                 m=mMap.addMarker(new MarkerOptions().position(point).title(String.valueOf(machinesList.get(i).machineID))
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)).draggable(true));
                 m.setTag(0);
             } else {
-                m = mMap.addMarker(new MarkerOptions().position(point).title(String.valueOf(machinesList.get(i).machineID)));
-                m.setTag(0);
-                m.setDraggable(isDraggable);
+                if(machinesList.get(i).status==9){
+                    m=mMap.addMarker(new MarkerOptions().position(point).title(String.valueOf(machinesList.get(i).machineID))
+                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)).draggable(true));
+                    m.setTag(0);
+                    m.setDraggable(isDraggable);
+                }else {
+                    m = mMap.addMarker(new MarkerOptions().position(point).title(String.valueOf(machinesList.get(i).machineID)));
+                    m.setTag(0);
+                    m.setDraggable(isDraggable);
+                }
             }
         }
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -70,17 +79,14 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startPoint,zoom));
         mMap.setMyLocationEnabled(true);
         mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerDragListener(this);
     }
-
-
 
     @Override
     public void onMapLongClick(LatLng latLng) {
         newPoint = latLng;
         startActivity(new Intent(this, ConfirmNewDevice.class));
-        //Toast.makeText(this,"!!!",Toast.LENGTH_LONG).show();
     }
-
 
     @Override
     public void onMarkerDragStart(Marker marker) {
@@ -94,11 +100,50 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLon
 
     @Override
     public void onMarkerDragEnd(Marker marker) {
-        Toast.makeText(this,"!!!",Toast.LENGTH_LONG).show();
+        ChangeStatus(marker);
     }
 
     @Override
     public void onInfoWindowClick(Marker marker) {
         Toast.makeText(this,"!!!",Toast.LENGTH_LONG).show();
+    }
+
+
+    private void ChangeStatus(final Marker marker){
+        dialogChangeStatus = new AlertDialog.Builder(this);
+
+        final String[] strStatus = {"Activated", "NotActivated"};
+        dialogChangeStatus.setTitle("Choose status for device");
+        dialogChangeStatus.setSingleChoiceItems(strStatus, 1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String status = strStatus[which];
+                switch (status) {
+                    case "Activated": intStatus = 4;
+                        break;
+                    case "NotActivated": intStatus = 5;
+                        break;
+
+                }
+
+            }
+        });
+
+        dialogChangeStatus.setPositiveButton("SET", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                DB_connect con = new DB_connect();
+                con.UpdateStatus(String.valueOf(marker.getTitle()), intStatus);
+            }
+        });
+        dialogChangeStatus.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        //Output
+        AlertDialog dialogStatusChange = dialogChangeStatus.create();
+        dialogStatusChange.show();
     }
 }
